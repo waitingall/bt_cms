b3427423082845c1dd24f211cddb554e94946612
Sang <sangdlh95@gmail.com>
2017-03-29 19:09:27 +0700

    NguyenDuyHoa
    NguyenDuyHoa

---
+++ blocks/12/12.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html style="background-color: #f3f3f4;">
+
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/12.less', 'css/12.css');
+        ?>
+        <link href="css/12.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+     <div class="row ">
+         <div class="vamtam-grid grid-1-1  vamtam-first-level grid-1-1  first vamtam-cover-bg has-background extended no-extended-padding" style="
+				background: url( 'https://vip-restaurant.vamtam.com/wp-content/uploads/2016/07/sushi-parallax-1.jpg' ) no-repeat ;
+				background-size: cover;
+			background-attachment: fixed;background-color:#4a4a4a;padding-top:80px;padding-bottom:80px;" id="vamtam-column-c3abd97e9477f03c1cab59520eea4fd2"><div class="limit-wrapper"><div class="extended-column-inner"><style>#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 p,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 em,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h1,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h2,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h3,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h4,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h5,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h6,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h1 a:not(:hover),#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h2 a:not(:hover),#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h3 a:not(:hover),#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h4 a:not(:hover),#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h5 a:not(:hover),#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 h6 a:not(:hover),#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 .column-title,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 .sep-text h2.regular-title-wrapper,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 .text-divider-double,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 .sep-text .sep-text-line,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 .sep,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 .sep-2,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 .sep-3,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 .portfolio-filters .inner-wrapper .cbp-filter-item,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 .portfolio-filters .inner-wrapper .cbp-filter-item:hover,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 .portfolio-filters .inner-wrapper .cbp-filter-item.cbp-filter-item-active,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 td,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 th,#vamtam-column-c3abd97e9477f03c1cab59520eea4fd2 caption{color:#fff;}</style>
+                     <div class="vamtam-blank-space " style="height:130px">
+                         
+                     </div>
+<div class="row ">
+    <div class="vamtam-grid grid-1-1  grid-1-1  first unextended animation-from-bottom animated-active no-extended-padding animation-ended" style="padding-top:0.05px;padding-bottom:0.05px;" id="vamtam-column-3a0377642a177f39cfc13af2eef556f4"> <div class="sep-text single centered">
+            <div class="sep-text-before"><div class="sep-text-line"></div>
+                
+            </div>
+<div class="content">
+<h2 class="text-divider-double">
+MENU
+</h2><div class="text-divider-subtitle">Today Specials</div> </div>
+<div class="sep-text-after"><div class="sep-text-line"></div></div>
+</div>
+</div></div>
+<div class="vamtam-blank-space " style="height:130px"></div></div></div></div></div>
+    </body>
+
+</html>
