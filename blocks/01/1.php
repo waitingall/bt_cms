8181d8a21a556b8b1489750552a883ff5df8867e
Thinh <Tra Thai Thinh>
2017-03-29 19:03:32 +0700

    Tra Thai Thinh
    Tra Thai Thinh

--- blocks/01/1.php
+++ blocks/01/1.php
@@ -24,6 +24,7 @@
 
 
         <div class='type-1'>
+            <div style="height: 50px;"></div>
             <div class='container'>
                 <div class='row'>
 
@@ -31,10 +32,34 @@
                     <div class='col-md-4 col-1'>
                         <img src='images/sushi-icon-1.svg'>
                         <h4 class='title'>Futomaki</h4>
-                        <p><span class="vamtam-blank-space  vamtam-hide-lowres"></span>
+                        <p><span></span>
                             Sushi rice and seafood, etc. rolled in dried seaweed sheets. There are countless varieties of sushi rolls differing in ingredients and thickness. Sushi rolls prepared “inside out” are very popular outside of Japan, but rarely found in Japan.</p>
                     </div>
-
+       
+                      <!--COL 2-->
+                    <div class='col-md-4 col-2' >
+                  
+                        <div class='col-2-top'>
+                        <h4> RESERVATIONS</h4>
+                        </div>
+                        <div class='col-2-bottom'>
+                            <h5> Making a reservation at Délicious restaurant is easy and takes just a couple of minutes.</h5>
+                            <h6> +359 562 958</h6>
+                            <p><a href="#">Book now</a></p>
+                         </div>
+                         
+                        <div class='col-2-hinh'></div>
+                       
+                        
+                    </div>
+                 <!--COL 3-->
+                    <div class='col-md-4 col-3'>
+                        <img src='images/sushi-icon-2.svg'>
+                        <h4 class='title'>Nigiri</h4>
+                        <p><span></span>
+                            Small rice balls with fish, shellfish, etc. on top. There are countless varieties of nigirizushi, some of the most common ones being tuna, shrimp, eel, squid, octopus and fried egg.</p>
+                            
+                    </div>
 
                 </div>
             </div>
