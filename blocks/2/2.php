a7d027dab2ad704407127a796817c4e9baeab4eb
waitingall <waitingal2@gmail.com>
2017-03-30 22:53:07 +0700

    dang hoang anh

---
+++ blocks/2/2.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    
+    <link href="css/2.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<div class="clearfix"></div>
+
+<!-- begin Welcome to Delicious -->
+<div class='welcome-2'>
+
+    <!-- begin mask -->
+    <div class="mask">
+        <div class='container'>
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-10">
+
+                    <!-- begin welcome -->
+                    <div class="welcome">
+                        <h4>Welcome to Delicious</h4>
+                        <p>“First and foremost I am a chef, whether behind the stove at one of my Northern California restaurants or for the past 15 years in front of the camera on my Food Network cooking shows. Creating new dishes and flavor combinations that bring cooks and our restaurant guests pleasure is my job and I love it.”</p>
+                        <p><a href="https://en.wikipedia.org/wiki/Tyler_Florence" target="_blank">M.R. Tyler Florence</a></p>
+                        <p><img src="images/sign.svg" alt="sign"></p>    
+                    </div>
+                    <!-- end welcome -->
+
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+    </div>
+    <!-- end mask -->
+
+</div>
+<!-- end Welcome to Delicious -->
+
+<div class="clearfix"></div>
+
+<!-- begin MENU Today Specials -->
+<div class='menu-2'>
+    <div class='container'>
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+
+                <!-- begin menu -->
+                <div class="menu">
+                    <div class="blank-space"></div>
+
+                    <!-- begin line -->
+                    <div class="line">
+
+                        <div class="line-left">
+                            <div class="text-line"></div>
+                        </div>
+
+                        <!-- begin content -->
+                        <div class="content">
+                            <h2>MENU</h2>
+                            <div class="today">Today Specials</div>
+                        </div>
+                        <!-- end content -->
+
+                        <div class="line-right">
+                            <div class="text-line"></div>
+                        </div>
+                        
+                    </div>
+                    <!-- end line -->
+
+                    <div class="blank-space"></div>
+                </div>
+                <!-- end menu -->
+
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </div>
+</div>
+<!-- end MENU Today Specials -->
+
+<div class="clearfix"></div>
+
+
+
+</body>
+</html>
