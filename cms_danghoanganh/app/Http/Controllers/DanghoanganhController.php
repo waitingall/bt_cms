4aa053b930682a0ca18a1a4eb48434607a3b034a
trathinh <trathaithinh@gmail.com>
2017-04-04 00:06:34 -0700

    Dang Hoang Anh
    Dang Hoang Anh

---
+++ cms_danghoanganh/app/Http/Controllers/DanghoanganhController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class DanghoanganhController extends Controller {
+
+    public function index() {
+        return view('danghoanganh.index');
+    }
+
+}
