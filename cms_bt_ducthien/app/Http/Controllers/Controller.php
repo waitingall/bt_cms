1494a4a3f425ac5b5b8ce7479f0726108b460800
thiennguyenduc <thiennguyenduc1999@gmail.com>
2017-04-04 23:20:11 +0700

    ducthien

--- cms_bt_ducthien/app/Http/Controllers/Controller.php
+++ cms_bt_ducthien/app/Http/Controllers/Controller.php
@@ -6,11 +6,22 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+
+use App\Http\Models\Tests.php;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     public function index() {
-    	var_dump(1);die();
+    	$obj = new Tests();
+        
+        $tests = $obj ->get_test();
+        
+        $data = array(
+            'tests' -> $tests
+        );
+        
+        return view('ducthien.index', $data);
     }
 }
