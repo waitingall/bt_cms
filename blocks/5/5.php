e5b35c1fda0b71046622bd0736f7d44a4d5fb574
pjle <pjle172@gmail.com>
2017-04-04 22:30:44 +0700

    Hình Số 5 (New Update Cố gắng hết sức)
    1

--- blocks/5/5.php
+++ blocks/5/5.php
@@ -2,87 +2,154 @@
 
 <html style="background-color: #f3f3f4;">
 
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <script src="js/jquery-3.1.1.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <style>
-            .carousel-inner > .item > img,
-            .carousel-inner > .item > a > img {
-                width: 70%;
-                margin: auto;
-            }
-        </style>
-        <?php
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+     <script src="js/owl.carousel.min.js"></script>
+    <style>
+        .carousel-inner > .item > img,
+        .carousel-inner > .item > a > img {
+            width: 70%;
+            margin: auto;
+        }
+    </style>
+   
+    <link href="css/5.css" rel="stylesheet" type="text/css" />
+
+     <?php
         if (!class_exists('lessc')) {
             include ('./libs/lessc.inc.php');
         }
         $less = new lessc;
         $less->compileFile('less/5.less', 'css/5.css');
         ?>
-        <link href="css/5.css" rel="stylesheet" type="text/css" />
 
-    </head>
+</head>
 
-    <body>
+<body>
 
 
-        <div class='type-5'>
-            <div class='container'>
-                <div class='row'>
-                    <!--COL 1-->
-                    <div id="myCarousel" class="carousel slide" data-ride="carousel" style="text-align: center;">
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                            <li data-target="#myCarousel" data-slide-to="1"></li>
-                            <li data-target="#myCarousel" data-slide-to="2"></li>
-                        </ol>
-
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item active">
-                                <img src="images/5.PNG" alt="" width="460" height="345">
-                                <h4 class="quote-summary">"Life tastes better with Délicious"</h4>
-                                <div class="quote-title">
-                                    <img src="images/SAO.PNG"> <span class="the-title">CHARLES F. LUMMIS</span>
-                                </div>
-                                <br>
-                                <div class="quote-content">Started off with the Mixed Seafood Sampler (Venetian Style). Foe our main dish we ordered the Vermicelli with Cuttle Fish Ink Sauce over Polenta and Tuscan Roast Lamb Chops. Everything was delicious.
+    <div class='type-5'>
+        <div class='container'>
+            <div class='row'>
+                <!--COL 1-->
+                <div class="limit-wrapper">
+                    <div class="row ">
+                        <div>
+                            <div class="row ">
+                                <div class="grid-1-1">
+                                    <p><img src="https://vip-restaurant.vamtam.com/wp-content/uploads/2016/07/sushi-icon-4.svg" alt="sushi-icon-4">
+                                    </p>
                                 </div>
                             </div>
-
-                            <div class="item">
-                                <img src="images/5.PNG" alt="" width="460" height="345">
-                                <h4 class="quote-summary">"Life tastes better with Délicious"</h4>
-                                <div class="quote-title">
-                                    <span class="the-title">CHARLES F. LUMMIS</span>
-                                </div>
-                                <div class="quote-content">Started off with the Mixed Seafood Sampler (Venetian Style). Foe our main dish we ordered the Vermicelli with Cuttle Fish Ink Sauce over Polenta and Tuscan Roast Lamb Chops. Everything was delicious.
+                            <div class="row ">
+                                <div class="owlCarousel">
+                                    <div class="vamtam-cubeportfolio ">
+                                        <div class="cbp-item-wrapper">
+                                            <blockquote class="clearfix">
+                                                <div class="quote-text">
+                                                    <h4 class="quote-summary">"Life tastes better with Délicious"</h4>
+                                                    <div class="quote-title-wrapper clearfix">
+                                                        <div class="quote-title"><span class="icon shortcode   use-hover"></span><span class="icon shortcode   use-hover"></span><span class="icon shortcode   use-hover"></span><span class="icon shortcode   use-hover" ></span><span class="icon shortcode   use-hover"></span> — <span class="the-title">CHARLES F. LUMMIS</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="quote-content">Out of all the place I have visited in the world, I have found Haddon to be the most beautiful and delightful. The delicious scones in Haddon's restaurant are the best I have had anywhere. The grounds are so magical I can almost imagine the days when my ancestors rode across them, armor and swords clanking and horses everywhere, as they rode off to fight wherever needed. Haddon has managed to give a magnificent glimpse into the past while still retaining all its charm. Thank you for such a delight.</div>
+                                                </div>
+                                            </blockquote>
+                                        </div>
+                                    </div>
+                                    <div class="cbp-item">
+                                        <div class="cbp-item-wrapper">
+                                            <blockquote class="clearfix">
+                                                <div class="quote-text">
+                                                    <h4 class="quote-summary">"Create with the heart; cook with love."</h4>
+                                                    <div class="quote-title-wrapper clearfix">
+                                                        <div class="quote-title"><span class="icon"></span><span class="icon shortcode   use-hover"></span><span class="icon shortcode   use-hover"></span><span class="icon shortcode   use-hover" ></span><span class="icon shortcode   use-hover"></span> — <span class="the-title">M.F. MOONZAJER</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="quote-content">Out of all the place I have visited in the world, I have found Haddon to be the most beautiful and delightful. The delicious scones in Haddon's restaurant are the best I have had anywhere. The grounds are so magical I can almost imagine the days when my ancestors rode across them, armor and swords clanking and horses everywhere, as they rode off to fight wherever needed. Haddon has managed to give a magnificent glimpse into the past while still retaining all its charm. Thank you for such a delight.!</div>
+                                                </div>
+                                            </blockquote>
+                                        </div>
+                                    </div>
+                                    <div class="cbp-item" style="width: 1260px; left: 2520px; top: 0px;">
+                                        <div class="cbp-item-wrapper">
+                                            <blockquote class="clearfix">
+                                                <div class="quote-text">
+                                                    <h4 class="quote-summary">“Good painting is like good cooking - it can be tasted, but not explained.”</h4>
+                                                    <div class="quote-title-wrapper">
+                                                        <div class="quote-title"><span class="icon shortcode   use-hover"></span><span class="icon shortcode   use-hover"></span><span class="icon shortcode   use-hover"></span><span class="icon shortcode   use-hover" ></span><span class="icon shortcode   use-hover"></span> — <span class="the-title">MALCOLM GLADWELL</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="quote-content">
+                                                        <p>Out of all the place I have visited in the world, I have found Haddon to be the most beautiful and delightful. The delicious scones in Haddon's restaurant are the best I have had anywhere. The grounds are so magical I can almost imagine the days when my ancestors rode across them, armor and swords clanking and horses everywhere, as they rode off to fight wherever needed. Haddon has managed to give a magnificent glimpse into the past while still retaining all its charm. Thank you for such a delight.</p>
+                                                    </div>
+                                                </div>
+                                            </blockquote>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-
-                            <div class="item">
-                                <img src="images/5.PNG" alt="" width="460" height="345">
-                                <h4 class="quote-summary">"Life tastes better with Délicious"</h4>
-                                <div class="quote-title">
-                                    <span class="the-title">CHARLES F. LUMMIS</span>
-                                </div>
-                                <div class="quote-content">Started off with the Mixed Seafood Sampler (Venetian Style). Foe our main dish we ordered the Vermicelli with Cuttle Fish Ink Sauce over Polenta and Tuscan Roast Lamb Chops. Everything was delicious.
+                            <div class="cbp-nav">
+                                <div class="cbp-nav-pagination">
+                                    <div class="cbp-nav-pagination-item"></div>
+                                    <div class="cbp-nav-pagination-item""></div>
+                                    <div class="cbp-nav-pagination-item""></div>
                                 </div>
                             </div>
-
+                        </div>
+                    </div>
+                </div>
+                <div class="row ">
+                    <div class="vamtam-grid grid-1-1">
+                        <div class="row ">
+                            <div class="vamtam-grid grid-1-1">
+                                <div class="TA_cdsratingsonlywide">
+                                    <div id="CDSRATINGWIDGET637" class="cdsROW white">
+                                        <div class="cdsROWContainer">
+                                            <div class="cdsComponent logo">
+                                                <a target="_blank" href=""><img src="https://www.tripadvisor.co.uk/img/cdsi/img2/branding/tripadvisor_logo_transp_340x80-18034-2.png">
+                                                </a>
+                                            </div>
+                                            <div  class="cdsComponent cdsLocName">Memento Cafe</a> </div>
+                                            <div id="CDSROWRATING637" class="cdsComponent cdsRating"> <img src="https://static.tacdn.com/img2/t4b/bldashboard/bubbleratings/4.0.png"> <span> 95 Reviews </span> </div>
+                                            <!--<![endif]-->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+    </div>
+    </div>
+    </div>
+
+ <script type="text/javascript"><!--
+$(document).ready(function() {
+   $('.owlCarousel').owlCarousel({
+  items: 1,
+  autoPlay: 50000,
+  navigation: true,
+
+  navigationText: false,
+  pagination: true
+});
+});
+--></script>
+<<style type="text/css" media="screen">
+  
+</style>
+</body>
 
 </html>
