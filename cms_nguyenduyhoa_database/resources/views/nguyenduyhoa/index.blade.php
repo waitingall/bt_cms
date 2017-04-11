957dd419691008abfccfd121013f97f8bf90929d
Hoaisme <hoaisme1995@gmail.com>
2017-04-11 11:49:45 +0700

    NguyenDuyHoa
    NguyenDuyHoa

--- cms_nguyenduyhoa_database/resources/views/nguyenduyhoa/index.blade.php
+++ cms_nguyenduyhoa_database/resources/views/nguyenduyhoa/index.blade.php
@@ -36,32 +36,40 @@
 
                       </thead>
     </table>
-       <div class="" style="
-				background: url( 'images/sushi-parallax-1.jpg' ) no-repeat ;
-				background-size: cover;
-			background-attachment: fixed;background-color:#4a4a4a;padding-top:80px;padding-bottom:80px;" id="vamtam-column-4a72034ab579d3ccada7e77c3dc801cf"><div class=""><div class=""><style>#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf p,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf em,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h1,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h2,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h3,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h4,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h5,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h6,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h1 a:not(:hover),#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h2 a:not(:hover),#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h3 a:not(:hover),#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h4 a:not(:hover),#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h5 a:not(:hover),#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf h6 a:not(:hover),#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf .column-title,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf .sep-text h2.regular-title-wrapper,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf .text-divider-double,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf .sep-text .sep-text-line,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf .sep,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf .sep-2,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf .sep-3,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf .portfolio-filters .inner-wrapper .cbp-filter-item,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf .portfolio-filters .inner-wrapper .cbp-filter-item:hover,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf .portfolio-filters .inner-wrapper .cbp-filter-item.cbp-filter-item-active,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf td,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf th,#vamtam-column-4a72034ab579d3ccada7e77c3dc801cf caption{color:#fff;}</style><div class="" style="height:130px"></div>
-				<div class="row ">
-					<div class="" style="padding-top:0.05px;padding-bottom:0.05px;" id="vamtam-column-7fe016dfa634ef0860cbc9ebc58ecfa2"> 
-						<div class="sep-text single centered">
-							<div class="sep-text-before">
-								<div class="sep-text-line"></div>
-							</div>
-							<div class="content">
-								<h2 class="text-divider-double">
-									Tea
-								</h2>
-								<div class="text-divider-subtitle">
-								Today Specials
-								</div> 
-							</div>
-							<div class="sep-text-after">
-								<div class="sep-text-line">
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="vamtam-blank-space " style="height:130px"></div></div></div></div>
+       <div class="vamtam-grid grid-1-1  grid-1-1  first vamtam-hide-bg-lowres has-background extended no-extended-padding" style="
+				background: url( 'images/waves-decoration.svg' ) repeat-x center top;
+				background-size: auto;
+			background-attachment: scroll;padding-top:200px;padding-bottom:200px;" id="vamtam-column-96738cfceefc27fd85b42ad18c895d21">
+			</div>
+    <div style="background: url( 'images/sushi-parallax-1.jpg' ) no-repeat ;
+						background-size: cover;
+						background-attachment: fixed;
+						background-color:#4a4a4a;
+						padding-top:80px;
+						padding-bottom:80px;">
+		<div class='container'>
+			<div class="row">
+                <div class="tea">
+                    <div class="blank-space"></div>
+                    <div class="sep">
+
+                        <div class="sep-text-before">
+                            <div class="sep-text-line"></div>
+                        </div>
+                        <div class="content">
+                            <h2>Tea</h2>
+                            <div class="today">Today Specials</div>
+                        </div>
+                        <div class="sep-text-after">
+                            <div class="sep-text-line"></div>
+                        </div>
+                    </div>
+                    <div class="blank-space"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
     </body>
 
 </html>
